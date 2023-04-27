681c6c05e456b08f6a0b04d2741dd7e34867dbfd
Vunguye1 <s364540@oslomet.no>
2023-04-27 13:28:48 +0200

    file_splitting function!

--- application.py
+++ application.py
@@ -36,16 +36,20 @@ def parse_flags(flags): # get the values of syn, ack and fin
 #                              Done header handling                             #
 # ------------------------------------------------------------------------------#
 
-def stop_and_wait_client(file_sent):
-    
-    data_list = []
+def file_splitting(list, file_sent):
     with open(str(file_sent), 'rb') as file:
         while True:
             data = file.read(1460) # take only 1460 bytes from picture
-            data_list.append(data) # add to an array
+            list.append(data) # add to an array
             if not data: # break if there is no more data
                 break
+
+def stop_and_wait_client(file_sent):
     
+    data_list = []
+    file_splitting(data_list, file_sent)
+    sequence_id = 0
+
     for i in range(len(data_list)):
         
         
